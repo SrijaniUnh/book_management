7a18afddac4c96f7270cc5f12c662c669b5ae05e
Archana14091999 <archana.kumbam@unh.edu>
2023-11-04 17:29:34 -0800

    create add_book view

--- book_management/bookstore/views.py
+++ book_management/bookstore/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Book
+from .forms import AddBookForm, UpdateBookForm
+from .external_api import fetch_book_details
 
-# Create your views here.
+def add_book(request):
+    if request.method == 'POST':
+        form = AddBookForm(request.POST)
+        if form.is_valid():
+            book = form.save(commit=False)
+            additional_details = fetch_book_details(book.title, book.author)
+            book.summary = additional_details.get('summary', '')
+            book.cover_url = additional_details.get('cover_url', '')
+            book.save()
+            return redirect('list_books')
+    else:
+        form = AddBookForm()
+
+    return render(request, 'bookstore/add_book.html', {'form': form})
