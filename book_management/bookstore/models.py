f7d794f54014d37ba2f186b0e3b20b90cdb58605
Archana14091999 <archana.kumbam@unh.edu>
2023-11-04 17:13:54 -0800

    create model

--- book_management/bookstore/models.py
+++ book_management/bookstore/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    publication_year = models.IntegerField()
+    summary = models.TextField(blank=True)
+    cover_url = models.URLField(blank=True)
+    rating = models.FloatField(blank=True, null=True)
