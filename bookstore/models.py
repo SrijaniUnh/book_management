58ca740e7e5d64b0facd5f6c32cbbd5067a35b38
SrijaniUnh <Srijani.Edara@unh.edu>
2023-11-06 18:36:05 -0500

    corrected project structure

--- bookstore/models.py
+++ bookstore/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 class Book(models.Model):
+    class Meta:
+        app_label = 'bookstore'
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
     publication_year = models.IntegerField()
